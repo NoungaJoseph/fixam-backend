02b718c295c878abc39d664d30edb67ea28245cf
NoungaJoseph <noungajoseph58@gmail.com>
2026-07-08 13:16:01 +0100

    fix: add localhost:5175 to CORS allowedOrigins to allow local website status polling

--- src/app.js
+++ src/app.js
@@ -59,7 +59,9 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:4000',
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175'
 ].filter(Boolean);
 
 app.use(cors({
